96a2844b85a2a27ffac8cb2ad94036185b2eb444
Dan Root <rootdan@gmail.com>
2018-11-24 09:11:53 +0000

    Make setup.py match tox and travis configs

--- setup.py
+++ setup.py
@@ -60,8 +60,8 @@ setup(
         'Topic :: Software Development :: Quality Assurance',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
